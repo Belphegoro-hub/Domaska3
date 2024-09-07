d0801b12c97ee11d815a6275b9d05962f7007073
Belphegoro-hub <Belphegoro030@yandex.ru>
2024-09-08 01:19:30 +0300

    Все!!!

--- Task1.java
+++ Task1.java
@@ -2,9 +2,9 @@ package saven;
 
 public class Task1 {
 	public static void main(String[] args) {
-		int[] list = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377};
-		for(int i = 0;i<list.length;i++) {
-			if(list[i] % 2 == 0) {
+		int[]list={0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377};
+		for(int i=0;i<list.length;i++) {
+			if(list[i]%2==0){
 				System.out.println(list[i]);
 			}
 		}
